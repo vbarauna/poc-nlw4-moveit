d88b445171b70f45008892fd946c8aff7a5acc8a
barauna <baraunasvinicius@gmail.com>
2021-02-23 23:42:33 -0300

    commit aula 2 23-02

---
+++ src/components/Profile.tsx
@@ -0,0 +1,17 @@
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile(){
+    return (
+        <div className={styles.profileContainer}>
+            <img src="https://github.com/vbarauna.png" alt="Vinicius Barauna"/>
+            <div>
+                <strong>Vinícius Barauna</strong>
+                <p>
+                    <img src="icons/level.svg" alt="level"/>
+                    Level 1
+                    </p>
+            </div>
+        </div>
+        
+    )
+}
