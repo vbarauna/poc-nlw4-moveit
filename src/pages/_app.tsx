5d71d9b7b783ccc1143453bc429e88f8bb5772d7
barauna <baraunasvinicius@gmail.com>
2021-02-24 23:41:39 -0300

    commit aula 3 24-02

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,16 @@
 
 import '../styles/global.css'
 
+import { ChallengesContext, ChallengesProvider } from '../contexts/ChallengesContext'
+import React from 'react'
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return (
+    <ChallengesProvider>
+      <Component {...pageProps} />
+    </ChallengesProvider>
+    )
 }
 
 export default MyApp
